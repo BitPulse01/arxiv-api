7c3bbbed1be569672a4aa52de0a155cab79f15f3
BitPulse01 <bitpulse0101@gmail.com>
2024-07-29 21:52:38 +0330

    #

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ class scraper:
 
         # print(self.SOUP)
     
-    def find_titles(self):
+    def find_titles_and_links(self):
         self.titles_elements = self.SOUP.select("p.title.is-5.mathjax")
+        self.links_element_parent = self.SOUP.select("p.list-title.is-inline-block")
+
+        self.links_element_parent
+
         self.titles = []
 
         for self.title in self.titles_elements:
-            self.titles.append(self.title.text.replace("\n", "", -1))
+            self.titles.append(self.title.text.replace("\n", "", -1).replace("  ","",-1))
         
         return self.titles
 
