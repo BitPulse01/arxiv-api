6d9a37f35c3dc1db274d2479bc6397f18c4ca0f5
BitPulse01 <bitpulse0101@gmail.com>
2024-07-29 13:52:48 +0330

    updated for searching titles

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ class scraper:
     def __init__(self, search_title: str) -> None:
         self.URL: str = f"https://arxiv.org/search/?query={search_title}&searchtype=all&abstracts=show&order=-announced_date_first&size=50&start=0"
 
-    def scrape_site(self):
         RESPONSE: requests.Response = requests.request("get", self.URL)
         RESPONSE_CONTENT = RESPONSE.content
         
         self.SOUP = BeautifulSoup(RESPONSE_CONTENT, "html.parser")
 
-        return self.SOUP
+        # print(self.SOUP)
+    
+    def find_titles(self):
+        self.titles_elements = self.SOUP.select("p.title.is-5.mathjax")
+        self.titles = []
+
+        for self.title in self.titles_elements:
+            self.titles.append(self.title.text)
+        
+        return self.titles
 
 
 search = str(input("Type smth to search : "))
 
 SCRAPER = scraper(search)
-SCRAPER_INFO = SCRAPER.scrape_site()
+SCRAPER_INFO = SCRAPER.find_titles()
 
 print(SCRAPER_INFO)
