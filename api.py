24ecb842f4afa0816c45ca3ef294959b48aa9c81
BitPulse01 <bitpulse0101@gmail.com>
2024-07-31 10:57:50 +0330

    Optional Query for field

--- api.py
+++ api.py
@@ -6,8 +6,16 @@ SERVER: FastAPI = FastAPI()
 
 
 @SERVER.get("/")
-async def search(search: str):
+async def search(search: str, field: str | None = None):
+    if field:
+        SCRAPER = scraper.scraper(search_title= search, field= field)
+        SCRAPER_INFO = SCRAPER.get_info()
+
+        return SCRAPER_INFO
+    
     SCRAPER = scraper.scraper(search_title= search)
     SCRAPER_INFO = SCRAPER.get_info()
 
     return SCRAPER_INFO
+
+    
