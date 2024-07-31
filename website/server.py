e4ca0bf05621ae2945f0480cfed7625bde1eeb46
BitPulse01 <bitpulse0101@gmail.com>
2024-07-31 12:53:06 +0330

    flask website - pt1

---
+++ website/server.py
@@ -0,0 +1,24 @@
+from flask import *
+import requests
+
+
+SERVER = Flask(__name__)
+
+@SERVER.route("/")
+def home():
+    return render_template("index.html")
+
+@SERVER.route("/search", methods=['GET', 'POST'])
+def search():
+    SEARCH = request.form.get("search")
+    FIELD = request.form.get("field")
+
+    URL = f"http://127.0.0.1:8000/?search={SEARCH}&field={FIELD}"
+    RESPONSE = requests.get(URL)
+    RESPONSE_JSON = RESPONSE.json()
+
+    return RESPONSE_JSON
+
+
+if __name__ == '__main__':
+    SERVER.run(debug= True)
